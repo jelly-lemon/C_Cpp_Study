9253215c2e89e35656f2f8952d4d3c91a720f6b4
nsfocus <lemeng1@qq.com>
2021-02-05 17:28:18 +0800

    daily commit

--- c/byte_alignment.c
+++ c/byte_alignment.c
@@ -1,9 +1,11 @@
 /**
  * 字节对齐
  *
- * char gender 占1个字节，int age 占4个字节，int n 占4个字节，
- * 总共9个字节，
- * 但为了对齐，该结构体总共占 12 个字节（按4对齐）
+ * 【解析】
+ * 原本以为会输出 5，结果输出 8
+ * char gender 占 1 个字节，int age 占 4 个字节，int n 占 4 个字节，
+ * 总共 9 个字节，
+ * 但为了对齐，该结构体总共占 12 个字节（按 4 对齐）
  */
 
 #include <stdio.h>
@@ -11,7 +13,6 @@
 struct Input {
     char gender;
     int age;
-    int n;
 };
 
 int main(void) {
