ac8e49ef7ab0c71b3b95354b7aceccdde84f2877
nsfocus <lemeng1@qq.com>
2021-02-26 16:41:14 +0800

    daily commit

--- c/binary_tree/tranverse_tree.c
+++ c/binary_tree/tranverse_tree.c
@@ -10,6 +10,35 @@ typedef struct Node {
     struct Node *lchild, *rchild;
 }BiTNode, *BiTree;
 
+// 链表，用来构建队列
+typedef struct link_list_Node {
+    BiTNode *tree_node_addr;
+    struct link_list_Node *next;
+}List_Node;
+
+void add(List_Node *pHead, BiTree *T) {
+    if (pHead == NULL)
+        return;
+
+    List_Node *p = pHead->next;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+
+    List_Node *new_node = (List_Node*)malloc(sizeof(List_Node));
+    new_node->tree_node_addr = *T;
+    p->next = new_node;
+}
+
+BiTNode *pop(List_Node *pHead) {
+    if (pHead == NULL || pHead->next == NULL)
+        return NULL;
+
+    List_Node *p = pHead->next;
+    pHead->next = p->next;
+
+    return p->tree_node_addr;
+}
 
 /**
  * 先序输入二叉树
@@ -39,24 +68,32 @@ void createBiTreeByPre(BiTree *T) {
 }
 
 /**
- * TODO 按层输入二叉树
+ * 按层输入二叉树
  *
  * @param T 指向根节点的指针变量的地址
  */
 void createBiTreeByLevel(BiTree *T) {
+    // TODO 被二维指针搞晕
     char ch;
+    List_Node *pHead = (List_Node*)malloc(sizeof(List_Node));
+    BiTNode *t;
 
-    if (T == NULL)
+    if (T == NULL || *T == NULL)
         return;
-    scanf("%c", &ch);
-    if (ch == '#')
-        *T = NULL;
-    else {
-        *T = (BiTree)malloc(sizeof(BiTNode));
-        if (*T == NULL)
-            exit(-1);
-        else {
-            (*T)->data = ch;
+
+
+    add(pHead, T);
+
+    while ((ch = getchar()) != '\n') {
+        t = pop(pHead);
+        // 只输入一个 # 表示空二叉树
+        if (ch == '#') {
+            t = NULL; // 根节点指针变量直接置为 NULL
+        } else {
+            t = (BiTNode*)malloc(sizeof(BiTNode));
+            t->data = ch;
+            add(pHead, t->lchild);
+            add(pHead, t->rchild);
         }
     }
 }
@@ -111,7 +148,7 @@ void LevelTraverse(BiTree T) {
 }
 
 int main() {
-    BiTree root;
+    BiTree root;    // 根节点
     createBiTreeByPre(&root);
     PreOrderTraverse(root);
     printf("\n");
--- c/data_structure/linked_list.c
+++ c/data_structure/linked_list.c
@@ -1,3 +1,9 @@
 /*
  * 实现一个单向链表
  */
+
+typedef struct Node {
+
+
+
+};
