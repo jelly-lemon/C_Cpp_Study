1824cc88d16a7c33d9abee5777cdb66732adebae
nsfocus <lemeng1@qq.com>
2021-05-19 09:35:00 +0800

    daily commit

--- Cpp/newcoder/NC109.cpp
+++ Cpp/newcoder/NC109.cpp
@@ -13,6 +13,44 @@
  */
 
 
-void test_0() {
+/**
+ * 思路 1：遍历二维数组，遇到 1 时 count++，表示岛屿数量，然后把该 1 周围的全部 1 置为 0
+ * 接着再继续遍历
+ *
+ * 运行时间：18ms
+超过20.49% 用C++提交的代码
+占用内存：760KB
+超过45.33%用C++提交的代码
+ */
+int solve(vector<vector<char> >& grid) {
+    // write code here
+    int nRow, nCol;
+    int count = 0;
+    nRow = grid.size();
+    nCol = grid[0].size();
+    for (int i = 0; i < nRow; i++) {
+        for (int j = 0; j < nCol; j++) {
+            if (grid[i][j] == '1') {
+                count++;
+                search(grid, i, j);
+            }
+        }
+    }
+    return count;
+}
 
+void search(vector<vector<char> >& grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
+        return ;
+    }
+    if (grid[i][j] == '1') {
+        grid[i][j] = '0';
+    } else if (grid[i][j] == '0'){
+        return ;
+    }
+
+    search(grid, i+1, j);
+    search(grid, i-1, j);
+    search(grid, i, j+1);
+    search(grid, i, j-1);
 }
