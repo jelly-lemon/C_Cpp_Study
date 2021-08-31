3735e858f86c7a4cbb908792e54ec342103faa2a
nsfocus-lm <lemeng1@qq.com>
2021-08-31 16:50:07 +0800

    daily commit

--- Cpp/newcoder/NC6.cpp
+++ Cpp/newcoder/NC6.cpp
@@ -1,11 +1,80 @@
-/*
+/**
+ * 二叉树的最大路径和
+ *
  * 给定一个二叉树，请计算节点值之和最大的路径的节点值之和是多少。
 这个路径的开始节点和结束节点可以是二叉树中的任意节点
-例如：
+
+ 例如：
 给出以下的二叉树，
         1
        / \
       2   3
 
-返回的结果为6
+返回的结果为 6，即：2 -> 1 -> 3
+
+ 示例 1
+输入：{-2,1}
+返回值：1
+
+ 示例 2
+ 输入：{-2,#,-3}
+     -2
+       \
+        -3
+
+返回值：-2
+
+ 难点：
+ - 任意起点，任意终点。起点不止是根节点，还有叶子节点。
+ - 节点中还有负数。
  */
+
+int getMaxPathValue(TreeNode *root) {
+    if (root == NULL) {
+        return INT_MIN;
+    }
+
+    // write code here
+    int leftMax = getMaxPathValue(root->left);
+    int rightMax = getMaxPathValue(root->right);
+
+    int max = 0;
+    if (leftMax > 0) {
+        max += leftMax;
+    }
+    if (root->val > 0) {
+        max += root->val;
+    }
+    if (rightMax > 0) {
+        max += rightMax;
+    }
+
+    // 说明三者都小于 0，矮子中挑高个子
+    if (max == 0) {
+        if (leftMax >= rightMax) {
+            max = leftMax;
+        } else {
+            max = rightMax;
+        }
+        if (max < root->val) {
+            max = root->val;
+        }
+    }
+
+    return max;
+}
+
+/**
+ * 思路：左子树最大路径和 + 根节点 + 右子树最大路径和
+ *
+ * 10/20 组用例通过
+ *
+ * 存在的问题：左子树 + 根节点 + 右子树 这三部分不一定能连成一个路径啊！
+ */
+int maxPathSum(TreeNode* root) {
+    if (root == NULL) {
+        return INT_MIN;
+    }
+
+    return getMaxPathValue(root);
+}
--- Cpp/newcoder/NC62.cpp
+++ Cpp/newcoder/NC62.cpp
@@ -1,4 +1,6 @@
 /*
+ * 平衡二叉树
+ *
  * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
 在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
 平衡二叉树（Balanced Binary Tree），
@@ -8,26 +10,38 @@
 
 
 /**
+ * 思路：递归判断，先获取左右子树的深度，再计算深度差，
+ * 以此来判断是否为平衡二叉树
+ *
  * 运行时间：3ms
 超过29.28%用C++提交的代码
 占用内存：504KB
 超过64.89%用C++提交的代码
  */
 bool IsBalanced_Solution(TreeNode* pRoot) {
+    // 如果是子树为空，则认为是平衡的
     if (pRoot == NULL)
         return true;
+
+    // 获取左右子树深度
     int r1 = getDepth(pRoot->left);
     int r2 = getDepth(pRoot->right);
-    if (abs(r1-r2) <= 1)
+    if (abs(r1-r2) <= 1) {
         return true;
-    else
-        return false;
+    }
+
+    return false;
 }
 
+/**
+ * 获取树的深度
+ */
 int getDepth(TreeNode* pRoot) {
     if (pRoot == NULL)
         return 0;
     int r1 = getDepth(pRoot->left) + 1;
     int r2 = getDepth(pRoot->right) + 1;
+
+    // 返回较大值
     return r1 > r2 ? r1 : r2;
 }
